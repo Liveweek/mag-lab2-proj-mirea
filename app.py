e6645b7e0109e5d2949bff1ea8b09aee6820e092
= <konakov-po@yandex-team.ru>
2023-09-20 22:55:17 +0300

    fix problems + api model

--- app.py
+++ app.py
@@ -4,14 +4,25 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-db = [
-    {
-        "id": 1,
-        "task_name": "Приготовить еду",
-        "task_description": "Надо на ужин приготовить еды, иначе будет грустно :(",
-        "is_completed": False
-    }
-]
+# db = [
+#     {
+#         "id": 1,
+#         "task_name": "Приготовить еду",
+#         "task_description": "Надо на ужин приготовить еды, иначе будет грустно :(",
+#         "is_completed": False
+#     }
+# ]
+
+
+class Increment:
+    value = 1
+    
+    def get_next(self):
+        self.value += 1
+        return self.value
+    
+    
+inc = Increment()
 
 
 class TodoTask(BaseModel):
@@ -19,6 +30,21 @@ class TodoTask(BaseModel):
     task_name: str
     task_description: str
     is_completed: bool
+    
+    
+class TodoCreate(BaseModel):
+    task_name: str
+    task_description: str
+
+
+db = [
+    TodoTask(
+        id=1,
+        task_name="Приготовить еду",
+        task_description="Надо на ужин приготовить еды, иначе будет грустно :(",
+        is_completed=False
+    )
+]
 
 
 @app.get("/hello")
@@ -33,27 +59,32 @@ def get_all_todos():
 
 @app.get("/todos/{id}", response_model=TodoTask)
 def get_todo_by_id(id: int):
-    todo_task = [todo for todo in db if todo["id"] == id][0]
-    return todo_task
+    return [todo for todo in db if todo.id == id][0]
 
 
 @app.post("/todos", response_model=list[TodoTask])
-def create_todo(todo: TodoTask):
-    db.append(todo.model_dump())
+def create_todo(todo: TodoCreate):
+    todo_obj = TodoTask(
+        id=inc.get_next(),
+        task_name=todo.task_name,
+        task_description=todo.task_description,
+        is_completed=False
+    )
+    db.append(todo_obj)
     return db
 
 
 @app.post("/todos/{id}/complete", response_model=list[TodoTask])
 def complete_task(id: int):
     for todo in db:
-        if todo["id"] == id and not todo["is_completed"]:
-            todo["is_completed"] = True
+        if todo.id == id and not todo.is_completed:
+            todo.is_completed = True
     return db
 
 
 @app.delete("/todos/{id}", response_model=TodoTask)
 def delete_task(id: int):
     for todo in db:
-        if todo["id"] == id:
+        if todo.id == id:
             db.remove(todo)
             return todo
